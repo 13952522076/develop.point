e8d32bf530651f9534c0364ca0c86c344a7158ff
13952522076 <xuma@my.unt.edu>
2021-05-15 02:47:42 -0400

    update

--- models/__init__.py
+++ models/__init__.py
@@ -4,8 +4,12 @@ from .pct import PCT
 from .Combine1 import combine1Amax
 from .Combine1 import combine1Cmax
 
+from .Combine12 import combine12Amax
+
 from .Combine1CUDA import combine1AmaxCUDA
 from .Combine1CUDA import combine1CmaxCUDA
+
+from .Combine1CUDA2 import combine1AmaxCUDA2
 
 from .pct2 import PCT2
 from .pct3 import PCT3
