7db1b7608680b696f3ee2640ed40eeb98bda32fc
13952522076 <xuma@my.unt.edu>
2021-05-26 15:43:44 -0400

    update

--- part_segmentation/train_partseg.py
+++ part_segmentation/train_partseg.py
@@ -123,11 +123,11 @@ def main(args):
         classname = m.__class__.__name__
         if classname.find('Conv2d') != -1:
             torch.nn.init.xavier_normal_(m.weight.data)
-            if m.bias.data:
+            if m.bias.data is not None:
                 torch.nn.init.constant_(m.bias.data, 0.0)
         elif classname.find('Linear') != -1:
             torch.nn.init.xavier_normal_(m.weight.data)
-            if m.bias.data:
+            if m.bias.data is not None:
                 torch.nn.init.constant_(m.bias.data, 0.0)
 
     try:
