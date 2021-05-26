20b3cd1093681e8c6390866b0173af694f107912
13952522076 <xuma@my.unt.edu>
2021-05-26 15:58:53 -0400

    update

--- part_segmentation/train.py
+++ part_segmentation/train.py
@@ -41,7 +41,7 @@ def to_categorical(y, num_classes):
     """ 1-hot encodes a tensor """
     new_y = torch.eye(num_classes)[y.cpu().data.numpy(),]
     if (y.is_cuda):
-        return new_y.cuda()
+        return new_y.cuda(non_blocking=True)
     return new_y
 
 
