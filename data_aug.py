c7e3d02a1bbc43145e40f4b9d9b8e2aa8e6382ea
13952522076 <xuma@my.unt.edu>
2021-05-27 21:32:16 -0400

    update no drop

---
+++ data_aug.py
@@ -0,0 +1,134 @@
+import os
+import glob
+import h5py
+import numpy as np
+import math
+from torch.utils.data import Dataset
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+def download():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget %s  --no-check-certificate; unzip %s' % (www, zipfile))
+        os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
+        os.system('rm %s' % (zipfile))
+
+def load_data(partition):
+    download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    all_data = []
+    all_label = []
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+        # print(f"h5_name: {h5_name}")
+        f = h5py.File(h5_name,'r')
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+def random_point_dropout(pc, max_dropout_ratio=0.875):
+    ''' batch_pc: BxNx3 '''
+    # for b in range(batch_pc.shape[0]):
+    dropout_ratio = np.random.random()*max_dropout_ratio # 0~0.875    
+    drop_idx = np.where(np.random.random((pc.shape[0]))<=dropout_ratio)[0]
+    # print ('use random drop', len(drop_idx))
+
+    if len(drop_idx)>0:
+        pc[drop_idx,:] = pc[0,:] # set to the first point
+    return pc
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+       
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.02):
+    N, C = pointcloud.shape
+    pointcloud += np.clip(sigma * np.random.randn(N, C), -1*clip, clip)
+    return pointcloud
+
+
+def rotation_pointcloud(pointcloud):
+    """
+    See: https://github.com/mauckc/rotation-matrix
+    Return the rotation matrix associated with counterclockwise rotation about
+    the user specified axis by theta radians.
+    """
+    # convert the input to an array
+    axis = (np.random.random([3])>0.6).astype(float)
+    axis = np.asarray(axis)
+    theta = np.random.random()
+    # Get unit vector of our axis
+    axis = axis/math.sqrt(np.dot(axis, axis))
+    # take the cosine of out rotation degree in radians
+    a = math.cos(theta/2.0)
+    # get the rest rotation matrix components
+    b, c, d = -axis*math.sin(theta/2.0)
+    # create squared terms
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    # create cross terms
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    # return our rotation matrix
+
+    rotation = np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
+                     [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
+                     [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+    return np.matmul(pointcloud, rotation)
+
+
+def scale_pointcloud(pointcloud):
+    scale = np.random.uniform(0.9, 1.1, [1, 3])
+    return  pointcloud*scale
+
+
+
+class ModelNet40(Dataset):
+    def __init__(self, num_points, partition='train', rotation=False, scale=False):
+        self.data, self.label = load_data(partition)
+        self.num_points = num_points
+        self.partition = partition
+        self.rotation = rotation
+        self.scale = scale
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        label = self.label[item]
+        if self.partition == 'train':
+            pointcloud = random_point_dropout(pointcloud) # open for dgcnn not for our idea  for all
+            pointcloud = translate_pointcloud(pointcloud)
+            np.random.shuffle(pointcloud)
+            if self.rotation:
+                pointcloud = rotation_pointcloud(pointcloud)
+            if self.scale:
+                pointcloud = scale_pointcloud(pointcloud)
+
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+if __name__ == '__main__':
+    train = ModelNet40(1024)
+    test = ModelNet40(1024, 'test')
+    # for data, label in train:
+    #     print(data.shape)
+    #     print(label.shape)
+    from torch.utils.data import DataLoader
+    train_loader = DataLoader(ModelNet40(partition='train', num_points=1024), num_workers=4,
+                              batch_size=32, shuffle=True, drop_last=True)
+    for batch_idx, (data, label) in enumerate(train_loader):
+        print(f"batch_idx: {batch_idx}  | data shape: {data.shape} | ;lable shape: {label.shape}")
+
---
+++ plot/plot_modelnet40_2.py
@@ -0,0 +1,65 @@
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+import random
+import numpy as np
+import h5py
+import math
+import sys
+sys.path.append("..")
+from data_aug import ModelNet40
+
+id = np.random.randint(0,2048)
+id=800 #airplane 11
+id=2001 # lighter
+id=1
+points=2048
+save_fig=True
+rotation=False
+scale=False
+
+
+datset = ModelNet40(points, partition='train', rotation=rotation, scale=scale)
+sample,label = datset.__getitem__(10)
+
+
+
+
+fig = pyplot.figure()
+ax = Axes3D(fig)
+
+sequence_containing_x_vals = sample[:, 0]
+x_min = min(sequence_containing_x_vals)
+x_max = max(sequence_containing_x_vals)
+print(f"x range: {x_max-x_min}")
+sequence_containing_y_vals = sample[:, 1]
+y_min = min(sequence_containing_y_vals)
+y_max = max(sequence_containing_y_vals)
+print(f"y range: {y_max-y_min}")
+sequence_containing_z_vals = sample[:, 2]
+z_min = min(sequence_containing_z_vals)
+z_max = max(sequence_containing_z_vals)
+print(f"z range: {z_max-z_min}")
+
+
+ax.scatter(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals)
+
+
+# make the panes transparent
+ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+# make the grid lines transparent
+ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+# Make panes transparent
+ax.set_xlim3d(x_min,x_max)
+ax.set_ylim3d(y_min,y_max)
+ax.set_zlim3d(z_min,z_max)
+
+ax.set_axis_off()
+ax.get_xaxis().get_major_formatter().set_useOffset(False)
+# pyplot.tight_layout()
+pyplot.show()
+if save_fig:
+    fig.savefig(f"{id}_{points}.pdf", bbox_inches='tight', pad_inches=0.05, transparent=True)
