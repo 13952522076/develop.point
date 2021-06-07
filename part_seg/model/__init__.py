958c177bff7314cadd58271a557f70fc8f58a776
13952522076 <xuma@my.unt.edu>
2021-06-07 03:56:40 -0400

    update

--- part_seg/model/__init__.py
+++ part_seg/model/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 # from .util import sample_and_group
-from .DGCNN_PAConv import PAConv
+# from .DGCNN_PAConv import PAConv
 from .model21 import model21H
 from .model21001 import model21001H
 from .modelA import ModelA
