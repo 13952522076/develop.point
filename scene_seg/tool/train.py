b27c5dba5c3db1da59c0827a965acd1c250e121f
13952522076 <xuma@my.unt.edu>
2021-06-08 01:58:47 -0400

    update

--- scene_seg/tool/train.py
+++ scene_seg/tool/train.py
@@ -16,7 +16,7 @@ from tensorboardX import SummaryWriter
 from util import dataset, transform
 from util.s3dis import S3DIS
 from util.util import AverageMeter, intersectionAndUnionGPU, get_logger, get_parser
-from model.pointnet2.paconv import PAConv
+# from model.pointnet2.paconv import PAConv
 
 
 def worker_init_fn(worker_id):
@@ -197,15 +197,15 @@ def train(train_loader, model, criterion, optimizer, epoch, correlation_loss):
 
         corr_loss = 0.0
         corr_loss_scale = args.get('correlation_loss_scale', 10.0)
-        if correlation_loss:
-            for m in model.module.SA_modules.named_modules():
-                if isinstance(m[-1], PAConv):
-                    kernel_matrice, output_dim, m_dim = m[-1].weightbank, m[-1].output_dim, m[-1].m
-                    new_kernel_matrice = kernel_matrice.view(-1, m_dim, output_dim).permute(1, 0, 2).reshape(m_dim, -1)
-                    cost_matrice = torch.matmul(new_kernel_matrice, new_kernel_matrice.T) / torch.matmul(
-                        torch.sqrt(torch.sum(new_kernel_matrice ** 2, dim=-1, keepdim=True)),
-                        torch.sqrt(torch.sum(new_kernel_matrice.T ** 2, dim=0, keepdim=True)))
-                    corr_loss += torch.sum(torch.triu(cost_matrice, diagonal=1) ** 2)
+        # if correlation_loss:
+        #     for m in model.module.SA_modules.named_modules():
+                # if isinstance(m[-1], PAConv):
+                #     kernel_matrice, output_dim, m_dim = m[-1].weightbank, m[-1].output_dim, m[-1].m
+                #     new_kernel_matrice = kernel_matrice.view(-1, m_dim, output_dim).permute(1, 0, 2).reshape(m_dim, -1)
+                #     cost_matrice = torch.matmul(new_kernel_matrice, new_kernel_matrice.T) / torch.matmul(
+                #         torch.sqrt(torch.sum(new_kernel_matrice ** 2, dim=-1, keepdim=True)),
+                #         torch.sqrt(torch.sum(new_kernel_matrice.T ** 2, dim=0, keepdim=True)))
+                #     corr_loss += torch.sum(torch.triu(cost_matrice, diagonal=1) ** 2)
         loss = main_loss + corr_loss_scale * corr_loss
         optimizer.zero_grad()
         loss.backward()
