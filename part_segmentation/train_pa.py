7eb0c3a87a53d23d88e2c8bf421bed5434aafaff
13952522076 <xuma@my.unt.edu>
2021-06-09 05:56:10 -0400

    update

--- part_segmentation/train_pa.py
+++ part_segmentation/train_pa.py
@@ -1,60 +1,40 @@
-"""
-Author: Benny
-Date: Nov 2019
-nohup python train.py --model model21A --normal --log_dir model21A > nohup/model21A.out &
-"""
-import argparse
+from __future__ import print_function
 import os
+import argparse
 import torch
-import datetime
-import logging
-import sys
-import random
-import importlib
-import shutil
-import provider
-import numpy as np
-import torch.backends.cudnn as cudnn
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR, StepLR
+from util.data_util import PartNormalDataset
 import torch.nn.functional as F
-
-
-from pathlib import Path
-from tqdm import tqdm
-from data_util import PartNormalDataset
+import torch.nn as nn
+from model.DGCNN_PAConv import PAConv
+import numpy as np
+from torch.utils.data import DataLoader
 from util.util import to_categorical, compute_overall_iou, load_cfg_from_cfg_file, merge_cfg_from_list, IOStream
+from tqdm import tqdm
 from tensorboardX import SummaryWriter
 from collections import defaultdict
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import CosineAnnealingLR, StepLR
 from torch.autograd import Variable
+import random
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = BASE_DIR
-sys.path.append(os.path.join(ROOT_DIR, 'models'))
+classes_str = ['aero', 'bag', 'cap', 'car', 'chair', 'ear', 'guitar', 'knife', 'lamp', 'lapt', 'moto', 'mug', 'Pistol',
+               'rock', 'stake', 'table']
 
 
-classes_str = ['aero','bag','cap','car','chair','ear','guitar','knife','lamp','lapt','moto','mug','Pistol','rock','stake','table']
+def get_parser():
+    parser = argparse.ArgumentParser(description='3D Shape Part Segmentation')
+    parser.add_argument('--config', type=str, default='dgcnn_paconv_train.yaml', help='config file')
+    parser.add_argument('opts', help='see config/dgcnn_paconv_train.yaml for all options', default=None,
+                        nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    assert args.config is not None
+    cfg = load_cfg_from_cfg_file(args.config)
+    if args.opts is not None:
+        cfg = merge_cfg_from_list(cfg, args.opts)
 
-def parse_args():
-    parser = argparse.ArgumentParser('Model')
-    parser.add_argument('--model', type=str, default='pointnet_part_seg', help='model name')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch Size during training')
-    parser.add_argument('--epochs', default=250, type=int, help='epoch to run')
-    parser.add_argument('--learning_rate', default=0.003, type=float, help='initial learning rate')
-    parser.add_argument('--optimizer', type=str, default='Adam', help='Adam or SGD')
-    parser.add_argument('--exp_name', type=str, default=None, help='log path')
-    parser.add_argument('--scheduler', type=str, default="step", help='log path')
-    parser.add_argument('--weight_decay', type=float, default=0, help='weight decay')
-    parser.add_argument('--momentum', type=float, default=0.9, help='weight decay')
-    parser.add_argument('--npoint', type=int, default=2048, help='point Number')
-    parser.add_argument('--normal', action='store_true', default=False, help='use normals')
-    parser.add_argument('--step', type=int, default=20, help='decay step for lr decay')
-    parser.add_argument('--manual_seed', type=int, default=0, help='manual_seed')
-    parser.add_argument('--lr_decay', type=float, default=0.5, help='decay rate for lr decay')
-    parser.add_argument('--model_type', type=str, default="insiou", help='choose to test the best insiou/clsiou/acc model')
-    return parser.parse_args()
+    cfg['manual_seed'] = cfg.get('manual_seed', 0)
+    cfg['workers'] = cfg.get('workers', 6)
+    return cfg
 
 
 def _init_():
@@ -63,11 +43,11 @@ def _init_():
     if not os.path.exists('checkpoints/' + args.exp_name):
         os.makedirs('checkpoints/' + args.exp_name)
 
-    # if not args.eval:  # backup the running files
-    #     os.system('cp main.py checkpoints' + '/' + args.exp_name + '/' + 'main.py.backup')
-    #     os.system('cp util/PAConv_util.py checkpoints' + '/' + args.exp_name + '/' + 'PAConv_util.py.backup')
-    #     os.system('cp util/data_util.py checkpoints' + '/' + args.exp_name + '/' + 'data_util.py.backup')
-    #     os.system('cp DGCNN_PAConv.py checkpoints' + '/' + args.exp_name + '/' + 'DGCNN_PAConv.py.backup')
+    if not args.eval:  # backup the running files
+        os.system('cp main.py checkpoints' + '/' + args.exp_name + '/' + 'main.py.backup')
+        os.system('cp util/PAConv_util.py checkpoints' + '/' + args.exp_name + '/' + 'PAConv_util.py.backup')
+        os.system('cp util/data_util.py checkpoints' + '/' + args.exp_name + '/' + 'data_util.py.backup')
+        os.system('cp DGCNN_PAConv.py checkpoints' + '/' + args.exp_name + '/' + 'DGCNN_PAConv.py.backup')
 
     global writer
     writer = SummaryWriter('checkpoints/' + args.exp_name)
@@ -94,16 +74,12 @@ def weight_init(m):
         torch.nn.init.constant_(m.bias, 0)
 
 
-
-
 def train(args, io):
-
     # ============= Model ===================
     num_part = 50
     device = torch.device("cuda" if args.cuda else "cpu")
 
-    MODELClass = importlib.import_module(args.model)
-    model = MODELClass.get_model(num_part).to(device)
+    model = PAConv(args, num_part).to(device)
     io.cprint(str(model))
 
     model.apply(weight_init)
@@ -136,10 +112,10 @@ def train(args, io):
     test_data = PartNormalDataset(npoints=2048, split='test', normalize=False)
     print("The number of test data is:%d", len(test_data))
 
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=8,
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.workers,
                               drop_last=True)
 
-    test_loader = DataLoader(test_data, batch_size=args.test_batch_size, shuffle=False, num_workers=8,
+    test_loader = DataLoader(test_data, batch_size=args.test_batch_size, shuffle=False, num_workers=args.workers,
                              drop_last=False)
 
     # ============= Optimizer ================
@@ -216,7 +192,6 @@ def train(args, io):
     torch.save(state, 'checkpoints/%s/model_ep%d.pth' % (args.exp_name, args.epochs))
 
 
-
 def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classes, io):
     train_loss = 0.0
     count = 0.0
@@ -225,7 +200,8 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
     metrics = defaultdict(lambda: list())
     model.train()
 
-    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(train_loader), total=len(train_loader), smoothing=0.9):
+    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(train_loader), total=len(train_loader),
+                                                            smoothing=0.9):
         batch_size, num_point, _ = points.size()
         points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(target.long()), \
                                           Variable(norm_plt.float())
@@ -237,9 +213,11 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
         seg_pred, loss = model(points, norm_plt, to_categorical(label, num_classes), target)  # seg_pred: b,n,50
 
         # instance iou without considering the class average at each batch_size:
-        batch_shapeious = compute_overall_iou(seg_pred, target, num_part)  # list of of current batch_iou:[iou1,iou2,...,iou#b_size]
+        batch_shapeious = compute_overall_iou(seg_pred, target,
+                                              num_part)  # list of of current batch_iou:[iou1,iou2,...,iou#b_size]
         # total iou of current batch in each process:
-        batch_shapeious = seg_pred.new_tensor([np.sum(batch_shapeious)], dtype=torch.float64)  # same device with seg_pred!!!
+        batch_shapeious = seg_pred.new_tensor([np.sum(batch_shapeious)],
+                                              dtype=torch.float64)  # same device with seg_pred!!!
 
         # Loss backward
         loss = torch.mean(loss)
@@ -249,15 +227,15 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
 
         # accuracy
         seg_pred = seg_pred.contiguous().view(-1, num_part)  # b*n,50
-        target = target.view(-1, 1)[:, 0]   # b*n
+        target = target.view(-1, 1)[:, 0]  # b*n
         pred_choice = seg_pred.contiguous().data.max(1)[1]  # b*n
         correct = pred_choice.eq(target.contiguous().data).sum()  # torch.int64: total number of correct-predict pts
 
         # sum
         shape_ious += batch_shapeious.item()  # count the sum of ious in each iteration
-        count += batch_size   # count the total number of samples in each iteration
+        count += batch_size  # count the total number of samples in each iteration
         train_loss += loss.item() * batch_size
-        accuracy.append(correct.item()/(batch_size * num_point))   # append the accuracy of each iteration
+        accuracy.append(correct.item() / (batch_size * num_point))  # append the accuracy of each iteration
 
         # Note: We do not need to calculate per_class iou during training
 
@@ -274,7 +252,7 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
     metrics['accuracy'] = np.mean(accuracy)
     metrics['shape_avg_iou'] = shape_ious * 1.0 / count
 
-    outstr = 'Train %d, loss: %f, train acc: %f, train ins_iou: %f' % (epoch+1, train_loss * 1.0 / count,
+    outstr = 'Train %d, loss: %f, train acc: %f, train ins_iou: %f' % (epoch + 1, train_loss * 1.0 / count,
                                                                        metrics['accuracy'], metrics['shape_avg_iou'])
     io.cprint(outstr)
     # Write to tensorboard
@@ -294,7 +272,8 @@ def test_epoch(test_loader, model, epoch, num_part, num_classes, io):
     model.eval()
 
     # label_size: b, means each sample has one corresponding class
-    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(test_loader), total=len(test_loader), smoothing=0.9):
+    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(test_loader), total=len(test_loader),
+                                                            smoothing=0.9):
         batch_size, num_point, _ = points.size()
         points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(target.long()), \
                                           Variable(norm_plt.float())
@@ -334,7 +313,8 @@ def test_epoch(test_loader, model, epoch, num_part, num_classes, io):
 
     for cat_idx in range(16):
         if final_total_per_cat_seen[cat_idx] > 0:  # indicating this cat is included during previous iou appending
-            final_total_per_cat_iou[cat_idx] = final_total_per_cat_iou[cat_idx] / final_total_per_cat_seen[cat_idx]  # avg class iou across all samples
+            final_total_per_cat_iou[cat_idx] = final_total_per_cat_iou[cat_idx] / final_total_per_cat_seen[
+                cat_idx]  # avg class iou across all samples
 
     metrics['accuracy'] = np.mean(accuracy)
     metrics['shape_avg_iou'] = shape_ious * 1.0 / count
@@ -363,8 +343,7 @@ def test(args, io):
     num_part = 50
     device = torch.device("cuda" if args.cuda else "cpu")
 
-    MODELClass = importlib.import_module(args.model)
-    model = MODELClass.get_model(num_part).to(device)
+    model = PAConv(args, num_part).to(device)
     io.cprint(str(model))
 
     from collections import OrderedDict
@@ -385,9 +364,11 @@ def test(args, io):
     total_per_cat_iou = np.zeros((16)).astype(np.float32)
     total_per_cat_seen = np.zeros((16)).astype(np.int32)
 
-    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(test_loader), total=len(test_loader), smoothing=0.9):
+    for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(test_loader), total=len(test_loader),
+                                                            smoothing=0.9):
         batch_size, num_point, _ = points.size()
-        points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(target.long()), Variable(norm_plt.float())
+        points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(
+            target.long()), Variable(norm_plt.float())
         points = points.transpose(2, 1)
         norm_plt = norm_plt.transpose(2, 1)
         points, label, target, norm_plt = points.cuda(non_blocking=True), label.squeeze().cuda(
@@ -426,16 +407,13 @@ def test(args, io):
         class_iou += total_per_cat_iou[cat_idx]
         io.cprint(classes_str[cat_idx] + ' iou: ' + str(total_per_cat_iou[cat_idx]))  # print the iou of each class
     avg_class_iou = class_iou / 16
-    outstr = 'Test :: test acc: %f  test class mIOU: %f, test instance mIOU: %f' % (metrics['accuracy'], avg_class_iou, metrics['shape_avg_iou'])
+    outstr = 'Test :: test acc: %f  test class mIOU: %f, test instance mIOU: %f' % (
+    metrics['accuracy'], avg_class_iou, metrics['shape_avg_iou'])
     io.cprint(outstr)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    if args.exp_name is None:
-        args.exp_name = args.model+str(datetime.datetime.now().strftime('-%Y%m%d%H%M%S'))
-    else:
-        args.exp_name = args.model+ "-" + args.msg
+    args = get_parser()
     _init_()
 
     if not args.eval:
@@ -463,5 +441,3 @@ if __name__ == "__main__":
         train(args, io)
     else:
         test(args, io)
-
-
