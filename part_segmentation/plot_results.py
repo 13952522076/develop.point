30265e011488702ddfcb18524020422c75040b0f
13952522076 <xuma@my.unt.edu>
2021-06-10 04:04:08 -0400

    update

--- part_segmentation/plot_results.py
+++ part_segmentation/plot_results.py
@@ -81,7 +81,9 @@ def parse_args():
 
 args = parse_args()
 
-
+cmap = pyplot.get_cmap('viridis')
+colors = cmap(np.linspace(0, 1, 50))
+np.random.shuffle(cmap)
 
 
 def plot_xyz(xyz, target, name="figures/figure.pdf"):
@@ -91,13 +93,10 @@ def plot_xyz(xyz, target, name="figures/figure.pdf"):
     x_vals = xyz[:, 0]
     y_vals = xyz[:, 1]
     z_vals = xyz[:, 2]
-
     ax.set_xlim3d(min(x_vals)*0.9, max(x_vals)*0.9)
     ax.set_ylim3d(min(y_vals)*0.9, max(y_vals)*0.9)
     ax.set_zlim3d(min(z_vals)*0.9, max(z_vals)*0.9)
-    cmap = pyplot.get_cmap('viridis')
-    colors = cmap(np.linspace(0, 1, 50))
-    print(colors.shape)
+
     for i in range(0,2048):
         col = target[i]
         print(col)
